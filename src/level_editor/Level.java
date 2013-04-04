d342ad4e1a90bd7be1a4035f9a6d1340d14aa12c
dannygoodman210 <dannygoodman210@gmail.com>
2013-04-03 21:32:13 -0400

    added methods to LevelEditor

--- src/level_editor/Level.java
+++ src/level_editor/Level.java
@@ -1,9 +1,22 @@
 package level_editor;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+import util.Sprite;
 
 public class Level implements Renderable{
 
+    private List<Sprite> mySprites;
+    
+    public Level(){
+        mySprites = new ArrayList<Sprite>();
+    }   
+    
+    public void addSprite(Sprite s){
+        mySprites.add(s);
+    }
+    
     @Override
     public Object getState () {
         // TODO Auto-generated method stub
