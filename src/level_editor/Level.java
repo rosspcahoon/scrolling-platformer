de1162f8e0ed6d93bbf7e78b1f030ce97e887455
Ross Cahoon <ross.p.cahoon@gmail.com>
2013-04-04 16:51:43 -0400

    Setup constants folder

--- src/level_editor/Level.java
+++ src/level_editor/Level.java
@@ -1,15 +1,23 @@
 package level_editor;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 import java.util.List;
+import util.PlatformerConstants;
+import util.Sprite;
 //import util.Sprite;
 import util.WorkspaceModel;
 import viewUtil.Renderable;
 
 public class Level extends WorkspaceModel{
-
-//    private List<Sprite> mySprites;
+   
+   private Dimension myFrameOfAction = new Dimension(PlatformerConstants.ACTION_FRAME_SIZE);
+   private Dimension myFrameOfReference = new Dimension(PlatformerConstants.REFERENCE_FRAME_SIZE);
+   private Sprite myPlayer;
+   private List<Sprite> mySprites;
+   private List<Sprite> myFrameOfActionSprites;
+   private List<Sprite> myFrameOfReferenceSprites;
     
     public Level(int id){
 //        mySprites = new ArrayList<Sprite>();
