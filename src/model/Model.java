4962beaef117451652e374f4dcd5adb91bf13766
Ross Cahoon <ross.p.cahoon@gmail.com>
2013-04-04 20:06:27 -0400

    Created model package and started Model.java

---
+++ src/model/Model.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.List;
+import level_editor.Level;
+
+
+/**
+ * Represents a scrolling platformer.
+ *  * 
+ * @author Ross Cahoon
+ */
+public class Model {
+    private List<Level> myLevels;
+    private Level myCurrLevel;
+    private View myView;
+
+
+    /**
+     * Create a game of the given size with the given display for its shapes.
+     */
+    public Model(View view) {
+        myView = view;  
+    }
+
+    /**
+     * Update game for this moment, given the time since the last moment.
+     */
+    public void update(double elapsedTime) {
+        Dimension bounds = myView.getSize();
+            updateSprites(elapsedTime, bounds, myView);
+
+    }
+
+    /**
+     * Draw all elements of the game.
+     */
+    public void paint(Graphics2D pen) {
+        myCurrLevel.paint(pen);
+    }
+
+}
